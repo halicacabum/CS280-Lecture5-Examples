41ceaf7946e93831891e2965c3fab2de38b79f30
halicacabum <halicacabum@gmail.com>
2017-03-21 15:51:17 +0800

    第五次課堂練習範例

---
+++ Example01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreLibrary;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Product product = new Product();
+            Console.Write("請輸入產品名稱:");
+            product.Name = Console.ReadLine();
+            Console.Write("請輸入{0}的價錢", product.Name);
+            Console.WriteLine("共有{0}個產品", Product.TotalProduct);
+            try
+            {
+                int price = 0;
+                price = int.Parse(Console.ReadLine());
+                product.SetPrice(price);
+                Console.WriteLine("產品:{0}的價格是{1}", product.Name, product.GetPrice());
+            }
+            catch
+            {
+                Console.WriteLine("價格輸入錯誤");
+                return;
+            }
+
+            Console.WriteLine(product);
+            Product product2 = product.Clone();
+            product2.Name = "BB";
+            Console.WriteLine(product2);
+            /*
+            Console.WriteLine("Product 的 name: {0}", product.Name);
+            //Product product2 = product;
+            Product product2 = new Product();
+            product2.Name = "B";
+            Console.WriteLine("Product 的 name: {0}", product.Name);
+            Console.WriteLine("Product 的 name: {0}", product2.Name);
+            */
+        }
+    }
---
+++ StoreLibrary/Product.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoreLibrary
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        private int price;
+        private float discount;
+
+        public static int TotalProduct { get; set; }
+
+        /*
+        public Product(string name, int price)
+        {
+            this.Price = price;
+            this.Name = name;
+            this.Discount = 1.0f;
+        }
+        */
+
+        public Product(string name, int price, float discount)
+        {
+            this.Price = price;
+            this.Name = name;
+            this.Discount = discount;
+            TotalProduct++;
+        }
+
+        public Product(string name, int price)
+            : this(name, price, 1.0f)
+        {
+        }
+
+        public Product()
+            : this("無產品名稱", 0, 1.0f)
+        {
+        }
+
+        public Product(Product product)
+            : this(product.Name, product.Price, product.Discount)
+        {
+        }
+
+        public int Price
+        {
+            get { return this.price; }
+
+            set
+            {
+                if (value > 200)
+                {
+                    this.price = 200;
+                }
+                else if (value < 39)
+                {
+                    this.price = 39;
+                }
+                else
+                {
+                    this.price = value;
+                }
+            }
+        }
+
+        public float Discount
+        {
+            get => this.discount;
+            set
+            {
+                if (value > 1.0f)
+                {
+                    this.discount = 1.0f;
+                }
+                else if (value < 0.3f)
+                {
+                    this.discount = 0.3f;
+                }
+                else
+                {
+                    this.discount = value;
+                }
+            }
+        }
+
+        public int GetPrice()
+        {
+            return this.price;
+        }
+
+        public void SetPrice(int price)
+        {
+            if (price > 200)
+            {
+                this.price = 200;
+            }
+            else if (price < 39)
+            {
+                this.price = 39;
+            }
+            else
+            {
+                this.price = price;
+            }
+        }
+
+        public Product Clone()
+        {
+            return new Product()
+        }
+
+        public override string ToString()
+        {
+            return string.Format("產品名稱:{0}\n產品價格:{1}\n產品折扣:{2}\n這是第{3}個Product"
+                 , this.Name
+                 , this.Price
+                 , this.Discount
+                 , Product.TotalProduct);
+        }
+    }
+}
